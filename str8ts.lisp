5a7801dec6c5d68dc130522084a9a78344745c34
Andre Fernandes <fpf.andre@gmail.com>
2022-02-08 14:18:29 -0300

    first commit

---
+++ str8ts.lisp
@@ -0,0 +1,97 @@
+(defun drop(n seq)
+    (if (= n 0) 
+        seq
+        (drop (- n 1) (rest seq))
+    )
+)
+
+(defun take(n seq)
+    (if (= n 0) 
+        ()
+    ; else 
+        (cons (first seq) (take (- n 1) (rest seq)))
+    )
+)
+
+
+(defun steps(n seq)
+    (if (null seq)
+        ()
+    ; else
+        (cons (first seq) (steps n (drop (- n 1) (rest seq) )))
+    )
+)
+
+(defun slice(start end step seq)
+    (if (null seq) 
+        ()
+    ; else
+        (steps step (drop start (take end seq))) 
+    )
+)
+
+(defun inList(el seq)
+    (if (null seq)
+        NIL
+    (if (= el (first seq))
+        T
+    ; else
+        (inList el (rest seq))
+    ))
+)
+
+(defun repeated(seq)
+    (if (null seq)
+        NIL
+    (if (inList (first seq) (rest seq))
+        T
+    ; else
+        (repeated (rest seq))   
+    ))
+)
+
+(defun repl(n el seq)
+    (if (= n 0)
+        (cons el (rest seq))
+        (cons (first seq) (repl (- n 1) el (rest seq)))
+    )
+)
+
+
+(defun main()
+    (write (repl 3 8 (list 0 1 2 3 4 5 6 7 1)))
+)
+
+(main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
